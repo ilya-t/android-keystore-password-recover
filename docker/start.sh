5e3199e00fcb572d6d2f55c5a5656020c3389c22
Tskhay Ilya <extraletters@gmail.com>
2019-02-09 19:08:55 +0300

    improve container start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,18 @@
-# Usage: pass path to keystore file as first argument!
+if [ "$#" -ne 2 ]; then
+  echo 'Usage: pass path to keystore file as first argument and' 
+  echo 'as second all other arguments for AndroidKeystoreBrute.jar (excluding path to keystore)'
+  echo 'For example:'
+  echo '    ./start.sh /path/to/keystore.file "-m 1 -savetail /app/keystore_location/tail.log 50"'
+  exit 1
+fi
+
 KEYSTORE_FILE=$1
+ARGS=$2
+
 PATH_TO_KEYSTORE=$(dirname $KEYSTORE_FILE)
 KEYSTORE_FILE_NAME=$(basename $KEYSTORE_FILE)
-METHOD=1
 
-CONTAINER_COMMAND="java -jar /app/android-keystore-password-recover/build/AndroidKeystoreBrute.jar -m $METHOD -k /app/keystore_location/$KEYSTORE_FILE_NAME -savetail /app/keystore_location/tail.log 50"
+CONTAINER_COMMAND="java -jar /app/android-keystore-password-recover/build/AndroidKeystoreBrute.jar -k /app/keystore_location/$KEYSTORE_FILE_NAME $ARGS"
 CONTAINER_NAME="keystore-recovery.$KEYSTORE_FILE_NAME"
 
 docker run --name $CONTAINER_NAME \
